5fe7c05eb0298ce063ebe63f47bfa9e36e79f695
dvirarazi0@gmail.com <dvirarazi0@gmail.com>
2025-07-14 07:46:11 +0300

    done fo now

---
+++ website/pages/index.ts
@@ -0,0 +1,11 @@
+import { send } from "../clientUtilities";
+
+var messageInput = document.querySelector("#messageInput") as HTMLInputElement;
+var sendButton = document.querySelector("#sendButton") as HTMLButtonElement;
+var responseDiv = document.querySelector("#responseDiv") as HTMLDivElement;
+
+sendButton.onclick = async () => {
+  var response = await send("message", messageInput.value);
+
+  responseDiv.innerText = response;
+};
